4b005c1676ef714d9e4b8b175ad46d05f92a1fec
bfmunozm <swsbmm@gmail.com>
2024-01-29 16:15:05 -0500

    feat: udistrital/sga_documentacion#22 Estandar de respuesta y errores en tiempo de ejecucion.

--- routers/router.go
+++ routers/router.go
@@ -9,11 +9,14 @@ package routers
 
 import (
 	"github.com/udistrital/sga_mid_practicas_academicas/controllers"
-
+	"github.com/udistrital/utils_oas/errorhandler"
 	"github.com/astaxie/beego"
 )
 
 func init() {
+
+	beego.ErrorController(&errorhandler.ErrorHandlerController{})
+
 	ns := beego.NewNamespace("/v1",
 		beego.NSNamespace("/practicas-academicas",
 			beego.NSInclude(
